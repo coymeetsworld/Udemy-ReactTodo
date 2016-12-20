d25964a482271bdd0cbd16144919ce124e813689
Coy Sanders <coymeetsworld@gmail.com>
2016-12-20 14:21:17 -0800

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -19,5 +19,31 @@ module.exports = {
 
 		/* Just to validate that the data is not malicious, we expect an array. */	
 		return $.isArray(todos) ? todos : [];
+	},
+	
+	filterTodos: function (todos, showCompleted, searchText) {
+		var filteredTodos = todos;
+		
+		// Filter by showCompleted
+		filteredTodos = todos.filter((todo) => {
+			return !todo.completed || showCompleted;
+		});
+		
+		// Filter by searchText
+		if (searchText.length > 0) {
+			filteredTodos = todos.filter((todo) => {
+				return todo.text.toLowerCase().indexOf(searchText.toLowerCase()) !== -1 ? true : false;
+			});
+		}
+		
+		
+		// Sort todos with non-completed first
+		filteredTodos.sort((a, b) => {
+			if (!a.completed && b.completed) { return -1; }		
+			else if (a.completed && !b.completed) { return 1; }
+			return 0;
+		});
+
+		return filteredTodos;
 	}
 };
