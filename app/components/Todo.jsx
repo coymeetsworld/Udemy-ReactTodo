7b5a43d997462d7aee5c242042c310e663b210e7
Coy Sanders <coymeetsworld@gmail.com>
2016-12-20 18:36:04 -0800

    Minor change in format of timestamp for todos

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -16,7 +16,7 @@ var Todo = React.createClass({
 				timestamp = completedAt;
 			}
 				
-			return message + moment.unix(timestamp).format('MMM Do , YYYY @ h:mm a');
+			return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
 		}
 
 		return (
