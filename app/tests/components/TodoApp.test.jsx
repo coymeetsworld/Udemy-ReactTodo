e8f7e977f729f724cf5f8adc850f5347ee002ce7
Coy Sanders <coymeetsworld@gmail.com>
2016-12-20 16:17:13 -0800

    Add completed and created timestamps with tests

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -24,13 +24,16 @@ describe('TodoApp', () => {
 		expect(todoApp.state.todos[0].text).toBe(todoText);
 		/* There should only be one item in todos array. */
 		expect(todoApp.state.todos.length).toBe(1);
+		expect(todoApp.state.todos[0].createdAt).toBeA('number');
 	});
 	
 	it('should toggle completed value when handleToggle called', () => {
 		var todoData = {				
 			id: 11,
 			text: 'Test features',
-			completed: false
+			completed: false,
+			createdAt: 0,
+			completedAt: undefined
 		};
 		var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
 		todoApp.setState({todos: [todoData]});
@@ -41,13 +44,33 @@ describe('TodoApp', () => {
 		// Verify the todo is not completed.
 		expect(todoApp.state.todos[0].completed).toBe(false);
 
-		// Verify completed flag toggles to true.
+		// Verify completed flag toggles to true and completedAt is a number (UNIX timestamp).
 		todoApp.handleToggle(11);
 		expect(todoApp.state.todos[0].completed).toBe(true);
+		expect(todoApp.state.todos[0].completedAt).toBeA('number');
+	});
 
-		// Verify completed flag toggles back to false.
+	it('should toggle todo from completed to incomplete', () => {
+		var todoData = {				
+			id: 11,
+			text: 'Test features',
+			completed: true,
+			createdAt: 0,
+			completedAt: undefined
+		};
+		var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+		todoApp.setState({todos: [todoData]});
+		
+		// Check to verify there's only one todo object in the list of todos.
+		expect(todoApp.state.todos.length).toBe(1);
+		
+		// Verify the todo is not completed.
+		expect(todoApp.state.todos[0].completed).toBe(true);
+
+		// Verify completed flag toggles back to false and completedAt is undefined.
 		todoApp.handleToggle(11);
 		expect(todoApp.state.todos[0].completed).toBe(false);
+		expect(todoApp.state.todos[0].completedAt).toNotExist();
 
 	});
 	
